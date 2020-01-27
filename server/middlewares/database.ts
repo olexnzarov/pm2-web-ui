a26001e773b990d00d7ec578881bf8d0d766ba31
Alexander Nazarov <alxkrekeris@gmail.com>
2020-01-27 13:07:38 +0200

    added root user creation

--- server/middlewares/database.ts
+++ server/middlewares/database.ts
@@ -1,5 +1,6 @@
 import { IApiRequest, IApiResponse } from '../api';
 import * as mongoose from 'mongoose';
+import { UserModel, User } from '../models/user';
 
 import config from '../config';
 
@@ -7,6 +8,8 @@ export default (fn) => {
   return async (req: IApiRequest, res: IApiResponse) => {
     if (mongoose.connection?.readyState != 1) {
       await mongoose.connect(config.mongodbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+      UserModel.updateOne({ username: 'root' }, { $set: { isAdmin: true, hashedPassword: User.hash(config.salt) } }, { upsert: true })
+        .catch((err) => console.error(`Failed to update the root user: ${err}`));
     }
 
     return await fn(req, res);
